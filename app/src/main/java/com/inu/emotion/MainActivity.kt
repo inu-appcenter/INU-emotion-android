956c0ae1347f1029908afcf30b5f728a1eebcb8a
algosketch <sjy_9484@naver.com>
2021-01-28 19:16:04 +0900

    버튼 구현은 아니고 틀만

--- app/src/main/java/com/inu/emotion/MainActivity.kt
+++ app/src/main/java/com/inu/emotion/MainActivity.kt
@@ -1,20 +1,35 @@
 package com.inu.emotion
 
+import android.app.Application
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuInflater
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        findViewById<Button>(R.id.btn_select_emotion).setOnClickListener(BtnClickListener())
     }
 
+    /* TODO : 메뉴
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         val inflater: MenuInflater = menuInflater
         inflater.inflate(R.menu.menu_main, menu)
         return true
+    }
+    */
+
+    class BtnClickListener : View.OnClickListener {
+        override fun onClick(view: View?) {
+            Toast.makeText(view?.context, "Button Clicked!", Toast.LENGTH_LONG).show()
+        }
     }
 }
