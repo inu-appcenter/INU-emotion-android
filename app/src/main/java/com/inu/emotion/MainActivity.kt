59115d585fad9411863c249865d8de56ecdd8aea
algosketch <sjy_9484@naver.com>
2021-01-28 17:01:40 +0900

    홈 화면 UI

--- app/src/main/java/com/inu/emotion/MainActivity.kt
+++ app/src/main/java/com/inu/emotion/MainActivity.kt
@@ -2,10 +2,19 @@ package com.inu.emotion
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu_main, menu)
+        return true
     }
 }
