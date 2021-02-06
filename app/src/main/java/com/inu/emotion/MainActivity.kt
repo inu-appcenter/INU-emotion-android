bb45cb26e257be1ff1665c554e2488dce5a500a0
algosketch <sjy_9484@naver.com>
2021-02-07 02:51:08 +0900

    필요한 각 액티비티 추가 및 홈 화면 버튼으로 연결

--- app/src/main/java/com/inu/emotion/MainActivity.kt
+++ app/src/main/java/com/inu/emotion/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.inu.emotion
 
-import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -15,15 +14,18 @@ class MainActivity : AppCompatActivity() {
 
         val clickListener = BtnClickListener()
         findViewById<Button>(R.id.btn_select_emotion).setOnClickListener(clickListener)
+        findViewById<Button>(R.id.btn_result).setOnClickListener(clickListener)
         findViewById<Button>(R.id.btn_betting_emotion).setOnClickListener(clickListener)
     }
 
     inner class BtnClickListener : View.OnClickListener {
         override fun onClick(view: View?) {
-            val intent = when(view!!.id) {
+            val intent = when(view?.id) {
                 // TODO : 홈 화면에서 버튼 클릭 -> 화면 전환에 연결되는 액티비티를 지정
                 R.id.btn_select_emotion -> Intent(view.context, SelectEmotionActivity::class.java)
-                R.id.btn_betting_emotion -> Intent(view.context, SelectEmotionActivity::class.java)
+                R.id.btn_result -> Intent(view.context, ResultActivity::class.java)
+                R.id.btn_betting_emotion -> Intent(view.context, BettingTemperatureActivity::class.java)
+
                 else -> throw Exception("BtnClickListener Error : unexpected id")
             }
             this@MainActivity.startActivity(intent)
---
+++ app/src/main/java/com/inu/emotion/SelectElement.kt
@@ -0,0 +1,11 @@
+package com.inu.emotion
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SelectElement : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_select_element)
+    }
+}
