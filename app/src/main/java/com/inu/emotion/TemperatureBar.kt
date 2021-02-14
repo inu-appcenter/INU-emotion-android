d82d63197f5f38cdf3909c2330a1aa6e15784221
algosketch <sjy_9484@naver.com>
2021-02-14 16:51:53 +0900

    온도계 색깔 넣기 확인 버튼 추가

--- app/src/main/java/com/inu/emotion/TemperatureBar.kt
+++ app/src/main/java/com/inu/emotion/TemperatureBar.kt
@@ -3,11 +3,20 @@ package com.inu.emotion
 import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
+import androidx.core.content.ContextCompat
 
-class TemperatureBar @JvmOverloads constructor(
+class TemperatureBar constructor(
         context: Context,
         attrs: AttributeSet
 ) : androidx.appcompat.widget.AppCompatSeekBar(context, attrs) {
+    val temperatureColor : ArrayList<Int> = ArrayList(5)
+    init {
+        temperatureColor.add(ContextCompat.getColor(context, R.color.under_20))
+        temperatureColor.add(ContextCompat.getColor(context, R.color.under_40))
+        temperatureColor.add(ContextCompat.getColor(context, R.color.under_60))
+        temperatureColor.add(ContextCompat.getColor(context, R.color.under_80))
+        temperatureColor.add(ContextCompat.getColor(context, R.color.over_80))
+    }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec)
@@ -29,17 +38,26 @@ class TemperatureBar @JvmOverloads constructor(
         super.onDraw(canvas)
 
         val size = resources.getDimensionPixelSize(R.dimen.bar_size)
-        var strokeSize = resources.getDimensionPixelSize(R.dimen.bar_stroke_size)
+        val strokeSize = resources.getDimensionPixelSize(R.dimen.bar_stroke_size)
         val paint = Paint()
 
 
         // 막대 그리기
         paint.style = Paint.Style.STROKE
         paint.strokeWidth = resources.getDimensionPixelSize(R.dimen.bar_stroke_size).toFloat()
-        paint.color = Color.RED
+
         val middle = (strokeSize / 2).toFloat() // y 좌표 보정
-        val start = 50F // x 좌표 보정
-        var curTemperature = start + (size - 90) * (this.progress / 100F)
-        canvas?.drawLine(start, middle, curTemperature, middle, paint)
+        var start = 50F // x 좌표 보정
+
+        val curTemperature = start + (size - 90) * (this.progress / 100F)
+        var end : Float
+
+        for(i in 0 until 5) {
+            if(this.progress <= i * 20) break
+            paint.color = temperatureColor[i]
+            end = start + (size - 90) * (20F / 100F)
+            canvas?.drawLine(start, middle, curTemperature, middle, paint)
+            start = end
+        }
     }
 }
