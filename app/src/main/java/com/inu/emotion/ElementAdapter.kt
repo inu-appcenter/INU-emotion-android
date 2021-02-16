4a3820bd73807a3ed709de9d597ce7b852073acb
algosketch <sjy_9484@naver.com>
2021-02-17 04:32:48 +0900

    요소 선택 화면 리사이클러뷰 세팅

--- app/src/main/java/com/inu/emotion/ElementAdapter.kt
+++ app/src/main/java/com/inu/emotion/ElementAdapter.kt
@@ -1,4 +1,53 @@
 package com.inu.emotion
 
-class ElementAdapter {
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class ElementAdapter(private val dataSet: ArrayList<ElementVO>) :
+        RecyclerView.Adapter<ElementAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(viewGroup.context)
+                .inflate(R.layout.item_elements, viewGroup, false)
+
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+        viewHolder.category.text = dataSet[position].category
+        viewHolder.element1.text = dataSet[position].elements[0]
+        viewHolder.element2.text = dataSet[position].elements[1]
+        viewHolder.element3.text = dataSet[position].elements[2]
+        viewHolder.element4.text = dataSet[position].elements[3]
+        viewHolder.element5.text = dataSet[position].elements[4]
+        viewHolder.element6.text = dataSet[position].elements[5]
+    }
+
+    override fun getItemCount() = dataSet.size
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val category: TextView
+        val element1: TextView
+        val element2: TextView
+        val element3: TextView
+        val element4: TextView
+        val element5: TextView
+        val element6: TextView
+
+        init {
+            category = view.findViewById(R.id.category)
+            element1 = view.findViewById(R.id.element1)
+            element2 = view.findViewById(R.id.element2)
+            element3 = view.findViewById(R.id.element3)
+            element4 = view.findViewById(R.id.element4)
+            element5 = view.findViewById(R.id.element5)
+            element6 = view.findViewById(R.id.element6)
+        }
+    }
+
+    data class ElementVO(val category: String, val elements: ArrayList<String>)
 }
--- app/src/main/java/com/inu/emotion/SelectElementActivity.kt
+++ app/src/main/java/com/inu/emotion/SelectElementActivity.kt
@@ -2,10 +2,33 @@ package com.inu.emotion
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.recyclerview.widget.RecyclerView
 
 class SelectElementActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_select_element)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.elements)
+        val dataSet = insertDataSet()
+        recyclerView.adapter = ElementAdapter(dataSet)
+    }
+
+    private fun insertDataSet() : ArrayList<ElementAdapter.ElementVO> {
+        return arrayListOf<ElementAdapter.ElementVO>(
+                ElementAdapter.ElementVO("음식",
+                        arrayListOf("집밥", "패스트푸드", "건강식", "외식", "배달", "디저트")),
+                ElementAdapter.ElementVO("인간관계",
+                        arrayListOf("가족", "친구", "연인", "동료", "동기", "선후배")),
+                ElementAdapter.ElementVO("학업",
+                        arrayListOf("시험", "과제/레포트", "성적", "공모전/대외활동", "자격증", "또뭐가있을까요")),
+                ElementAdapter.ElementVO("취미",
+                        arrayListOf("영화/드라마", "게임", "독서", "여행", "요리", "휴식")),
+                ElementAdapter.ElementVO("건강",
+                        arrayListOf("운동", "수면", "질병", "스트레스", "체중", "또뭐가있을까요")),
+                ElementAdapter.ElementVO("기타",
+                        arrayListOf("업무", "날씨", "교통", "행운", "불운", "그냥")),
+        )
     }
 }
