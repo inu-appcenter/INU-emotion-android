36959ae951372326e1e611274413754ecb1b038c
algosketch <sjy_9484@naver.com>
2021-02-19 12:46:54 +0900

    리사이클러뷰 선택 가능

--- app/src/main/java/com/inu/emotion/ElementAdapter.kt
+++ app/src/main/java/com/inu/emotion/ElementAdapter.kt
@@ -1,14 +1,18 @@
 package com.inu.emotion
 
-import android.util.Log
+import android.graphics.Typeface
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 
 class ElementAdapter(private val dataSet: ArrayList<ElementVO>) :
         RecyclerView.Adapter<ElementAdapter.ViewHolder>() {
+    var elements : ArrayList<String> = ArrayList<String>()
+    var cntOfElement : Int = 0
+    get() = elements.size
 
     override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(viewGroup.context)
@@ -29,7 +33,7 @@ class ElementAdapter(private val dataSet: ArrayList<ElementVO>) :
 
     override fun getItemCount() = dataSet.size
 
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val category: TextView
         val element1: TextView
         val element2: TextView
@@ -46,6 +50,36 @@ class ElementAdapter(private val dataSet: ArrayList<ElementVO>) :
             element4 = view.findViewById(R.id.element4)
             element5 = view.findViewById(R.id.element5)
             element6 = view.findViewById(R.id.element6)
+            setOnClickListener()
+        }
+
+        fun setOnClickListener() {
+            val onClickListener = OnClickListener()
+            element1.setOnClickListener(onClickListener)
+            element2.setOnClickListener(onClickListener)
+            element3.setOnClickListener(onClickListener)
+            element4.setOnClickListener(onClickListener)
+            element5.setOnClickListener(onClickListener)
+            element6.setOnClickListener(onClickListener)
+        }
+
+        inner class OnClickListener() : View.OnClickListener {
+            override fun onClick(view: View?) {
+                if((view as TextView)?.typeface == Typeface.DEFAULT) {
+                    // 이미 3개를 선택했을 경우
+                    if(cntOfElement == 3) {
+                        Toast.makeText(view?.context, "최대 3개까지 선택 가능합니다.", Toast.LENGTH_SHORT).show()
+                        return
+                    }
+
+                    elements.add(view.text.toString())
+                    view.typeface = Typeface.DEFAULT_BOLD
+                }
+                else {
+                    elements.remove(view.text.toString())
+                    view.typeface = Typeface.DEFAULT
+                }
+            }
         }
     }
 
