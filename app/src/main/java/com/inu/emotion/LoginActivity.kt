b60b442a00857b11303fc6cf63192df7e4d441ba
윤지연 <wldus3979@naver.com>
2021-02-24 16:08:19 +0900

    로그인화면 버튼 하나와 텍스트뷰 2개(로그인 전 사라졌다가 틀렸을 때 생기는거 포함)

--- app/src/main/java/com/inu/emotion/LoginActivity.kt
+++ app/src/main/java/com/inu/emotion/LoginActivity.kt
@@ -2,10 +2,18 @@ package com.inu.emotion
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class LoginActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        findViewById<Button>(R.id.btn_login).setOnClickListener{
+            it.visibility=View.VISIBLE
+        }
+
     }
 }
+
