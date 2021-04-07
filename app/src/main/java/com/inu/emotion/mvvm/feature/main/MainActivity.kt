e099353094f3aec6dd6c784f371dcaa65222bd72
algosketch <sjy_9484@naver.com>
2021-04-08 03:02:25 +0900

    메인 액티비티, 메인 뷰모델 리펙터링

--- app/src/main/java/com/inu/emotion/mvvm/feature/main/MainActivity.kt
+++ app/src/main/java/com/inu/emotion/mvvm/feature/main/MainActivity.kt
@@ -37,29 +37,11 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initMainActivity() {
-        val clickListener = BtnClickListener()
+        val clickListener = viewModel.BtnClickListener(this)
         findViewById<Button>(R.id.btn_select_emotion).setOnClickListener(clickListener)
         findViewById<Button>(R.id.btn_result).setOnClickListener(clickListener)
         findViewById<Button>(R.id.btn_betting_emotion).setOnClickListener(clickListener)
         findViewById<Button>(R.id.btn_emotion_graph).setOnClickListener(clickListener)
         findViewById<ImageView>(R.id.image_profile).setOnClickListener(clickListener)
-    }
-
-    inner class BtnClickListener : View.OnClickListener {
-        override fun onClick(view: View?) {
-            val intent = when(view?.id) {
-                R.id.btn_select_emotion     -> Intent(view.context, SelectEmotionActivity::class.java)
-                R.id.btn_result             -> Intent(view.context, ResultActivity::class.java)
-                R.id.btn_betting_emotion    -> Intent(view.context, BettingTemperatureActivity::class.java)
-                R.id.btn_emotion_graph      -> {
-                    Toast.makeText(applicationContext, "준비중입니다.", Toast.LENGTH_LONG).show()
-                    null
-                }
-                R.id.image_profile          -> Intent(view.context, LoginActivity::class.java)
-                else                        -> throw Exception("BtnClickListener Error : unexpected id")
-            }
-
-            if(intent != null) this@MainActivity.startActivity(intent)
-        }
     }
 }
